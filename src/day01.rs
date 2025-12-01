12053f3624e4459c0842ba4de5e68f65210a4a73
B1n-ry <b1nry.dev@gmail.com>
2025-12-01 16:09:33 +0100

    Day 01 done

--- src/day01.rs
+++ src/day01.rs
@@ -14,11 +14,54 @@ pub fn solve() {
 }
 
 fn solve_part1(input: &str) -> String {
-    // TODO: Implement part 1 solution
-    format!("Not implemented yet. Input has {} characters.", input.len())
+    let mut zero_counter = 0;
+    let mut dial = 50;
+    for line in input.lines() {
+        let direction = line.chars().nth(0).expect("Malformed input");
+        let direction: i16 = match direction {
+            'L' => -1,
+            'R' => 1,
+            c => panic!("Incorrect character: {}", c)
+        };
+        let movement = line[1..].parse::<i16>().expect("Movement of dial not expressed as number");
+        dial = (100 + dial + movement * direction) % 100;
+
+        if dial == 0 {
+            zero_counter += 1;
+        }
+    }
+
+    format!("Solution to problem 1: {}", zero_counter)
 }
 
 fn solve_part2(input: &str) -> String {
-    // TODO: Implement part 2 solution
-    format!("Not implemented yet. Input has {} characters.", input.len())
+    let mut zero_counter = 0;
+    let mut dial = 50;
+    for line in input.lines() {
+        let direction = line.chars().nth(0).expect("Malformed input");
+        let direction: i32 = match direction {
+            'L' => -1,
+            'R' => 1,
+            c => panic!("Incorrect character: {}", c)
+        };
+        let movement = line[1..].parse::<i32>().expect("Movement of dial not expressed as number");
+
+        if movement >= 100 {
+            zero_counter += movement / 100;
+        }
+        let movement = movement % 100;
+        if movement == 0 {
+            continue;
+        }
+
+        let prev = dial;
+        dial += movement * direction;
+
+        if dial >= 100 || (dial <= 0 && prev != 0) {
+            zero_counter += 1;
+        }
+        dial = (dial + 100) % 100;
+    }
+
+    format!("Solution to problem 1: {}", zero_counter)
 }
