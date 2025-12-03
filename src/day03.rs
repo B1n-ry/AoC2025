9d45e1d9a4ee7306a5bb0be0b6607d4d408902a0
B1n-ry <b1nry.dev@gmail.com>
2025-12-03 09:56:55 +0100

    Solved day 3 part 1

--- src/day03.rs
+++ src/day03.rs
@@ -14,8 +14,28 @@ pub fn solve() {
 }
 
 fn solve_part1(input: &str) -> String {
-    // TODO: Implement part 1 solution
-    format!("Not implemented yet. Input has {} characters.", input.len())
+    let sum = input.lines().map(|bank| {
+        let jolts: Vec<u32> = bank
+            .chars()
+            .map(|c| c.to_digit(10).expect("Malformed input: Char not a digit"))
+            .collect();
+        let (Some(i), Some(highest)) = jolts
+            .iter()
+            .take(jolts.len() - 1)
+            .enumerate()
+            .fold((None, None), |(idx, max), (i, &curr)| {
+                if max < Some(curr) {
+                    (Some(i), Some(curr))
+                } else {
+                    (idx, max)
+                }
+        }) else {
+            panic!("Bank empty?");
+        };
+        let &next_high = jolts[(i + 1)..].iter().max().expect("Could not find 2nd highest");
+        format!("{}{}", highest, next_high).parse::<u32>().expect("Could not parse digits")
+    }).sum::<u32>();
+    format!("Total jolts used: {}", sum)
 }
 
 fn solve_part2(input: &str) -> String {
