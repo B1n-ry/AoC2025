9d5560114e90b7e5d8c0f463e3ea7b0b4e0e0410
B1n-ry <b1nry.dev@gmail.com>
2025-12-03 10:48:31 +0100

    Cleaned up code in day 3

--- src/day03.rs
+++ src/day03.rs
@@ -5,11 +5,11 @@ pub fn solve() {
     let input = utils::read_input(3);
     
     // Part 1
-    let part1 = solve_part1(&input);
+    let part1 = solve_part(&input, 2);
     println!("Part 1: {}", part1);
     
     // Part 2
-    let part2 = solve_part2(&input);
+    let part2 = solve_part(&input, 12);
     println!("Part 2: {}", part2);
 }
 
@@ -29,22 +29,7 @@ fn find_max_indexed<T: PartialOrd>(num_array: &[T]) -> Option<(usize, &T)> {
     Some((i, highest))
 }
 
-fn solve_part1(input: &str) -> String {
-    let sum = input.lines().map(|bank| {
-        let jolts: Vec<u32> = bank
-            .chars()
-            .map(|c| c.to_digit(10).expect("Malformed input: Char not a digit"))
-            .collect();
-        let (i, &highest) = find_max_indexed(&jolts[..(jolts.len() - 1)]).expect("Bank empty?");
-        
-        let &next_high = jolts[(i + 1)..].iter().max().expect("Could not find 2nd highest");
-        format!("{}{}", highest, next_high).parse::<u32>().expect("Could not parse digits")
-    }).sum::<u32>();
-    format!("Total jolts used: {}", sum)
-}
-
-fn solve_part2(input: &str) -> String {
-    let prob_2_n: usize = 12;
+fn solve_part(input: &str, battery_count: usize) -> String {
     let sum = input.lines().map(|bank| {
         let jolts: Vec<u32> = bank
             .chars()
@@ -52,8 +37,8 @@ fn solve_part2(input: &str) -> String {
             .collect();
 
         let mut next_idx = 0;
-        let activations: Vec<u32> = (1..=prob_2_n).map(|i| {
-            let sub_vec = &jolts[next_idx..(jolts.len() - (prob_2_n - i))];
+        let activations: Vec<u32> = (1..=battery_count).map(|i| {
+            let sub_vec = &jolts[next_idx..(jolts.len() - (battery_count - i))];
             let (i, &highest) = find_max_indexed(sub_vec).expect("Bank empty?");
             next_idx += i + 1;
             highest
