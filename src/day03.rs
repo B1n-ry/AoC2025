297bb0175e348421c8b1687748775a114a81c8ba
B1n-ry <b1nry.dev@gmail.com>
2025-12-03 10:03:56 +0100

    Start on day 3 part 2

--- src/day03.rs
+++ src/day03.rs
@@ -13,25 +13,30 @@ pub fn solve() {
     println!("Part 2: {}", part2);
 }
 
+fn find_max_indexed<T: PartialOrd>(num_array: &[T]) -> Option<(usize, &T)> {
+    let (Some(i), Some(highest)) = num_array
+        .iter()
+        .enumerate()
+        .fold((None, None), |(idx, max), (i, curr)| {
+            if max < Some(curr) {
+                (Some(i), Some(curr))
+            } else {
+                (idx, max)
+            }
+    }) else {
+        return None;
+    };
+    Some((i, highest))
+}
+
 fn solve_part1(input: &str) -> String {
     let sum = input.lines().map(|bank| {
         let jolts: Vec<u32> = bank
             .chars()
             .map(|c| c.to_digit(10).expect("Malformed input: Char not a digit"))
             .collect();
-        let (Some(i), Some(highest)) = jolts
-            .iter()
-            .take(jolts.len() - 1)
-            .enumerate()
-            .fold((None, None), |(idx, max), (i, &curr)| {
-                if max < Some(curr) {
-                    (Some(i), Some(curr))
-                } else {
-                    (idx, max)
-                }
-        }) else {
-            panic!("Bank empty?");
-        };
+        let (i, &highest) = find_max_indexed(&jolts[..(jolts.len() - 1)]).expect("Bank empty?");
+        
         let &next_high = jolts[(i + 1)..].iter().max().expect("Could not find 2nd highest");
         format!("{}{}", highest, next_high).parse::<u32>().expect("Could not parse digits")
     }).sum::<u32>();
