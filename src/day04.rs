247939124b796d7a954da4afd148e7a6e5a064be
B1n-ry <b1nry.dev@gmail.com>
2025-12-04 15:22:34 +0100

    Finished day 4

--- src/day04.rs
+++ src/day04.rs
@@ -1,4 +1,6 @@
-use crate::utils;
+use std::{thread::sleep, time::Duration};
+
+use crate::utils::{self, Grid};
 
 pub fn solve() {
     println!("Day 04");
@@ -13,12 +15,77 @@ pub fn solve() {
     println!("Part 2: {}", part2);
 }
 
+fn get_neighbour_rolls(grid: &Grid<char>, x: usize, y: usize) -> Vec<(usize, usize)> {
+    let mut neighbour_pos = Vec::new();
+    for x_i in x.saturating_sub(1)..=x.saturating_add(1) {
+        for y_i in y.saturating_sub(1)..=y.saturating_add(1) {
+            if x_i == x && y_i == y {
+                continue;
+            }
+            if grid.get(x_i, y_i) == Some(&'@') {
+                neighbour_pos.push((x_i, y_i));
+            }
+        }
+    }
+    neighbour_pos
+}
+
+fn get_neighbour_lists(grid: &Grid<char>) -> Vec<(usize, usize, Vec<(usize, usize)>)> {
+    grid.iter().filter_map(|(x, y, &item)| {
+        if item != '@' { return None; }
+
+        let neighbour_rolls = get_neighbour_rolls(&grid, x, y);
+
+        if neighbour_rolls.len() < 4 {
+            Some((x, y, neighbour_rolls))
+        } else {
+            None
+        }
+    }).collect()
+}
+
 fn solve_part1(input: &str) -> String {
-    // TODO: Implement part 1 solution
-    format!("Not implemented yet. Input has {} characters.", input.len())
+    let grid: Grid<char> = input.parse().expect("Not proper grid");
+    let accessible = get_neighbour_lists(&grid);
+
+    format!("There are {} accessible paper rolls", accessible.len())
 }
 
 fn solve_part2(input: &str) -> String {
-    // TODO: Implement part 2 solution
-    format!("Not implemented yet. Input has {} characters.", input.len())
+    let mut removed = 0;
+    let mut grid: Grid<char> = input.parse().expect("Not proper grid");
+
+    let mut remove = get_neighbour_lists(&grid);
+    let mut checkable = Vec::new();
+    loop {
+        if remove.len() == 0 {
+            break;
+        }
+        let mut removed_poses = Vec::new();
+        let mut remove_count = 0;
+        for (x, y, next) in &remove {
+            if !removed_poses.contains(&(*x, *y)) {
+                remove_count += 1;
+                removed_poses.push((*x, *y));
+                grid.set(*x, *y, 'x');
+            }
+            for &pos in next {
+                if !checkable.contains(&pos) {
+                    checkable.push(pos);
+                }
+            }
+        }
+        removed += remove_count;
+        remove.clear();
+        for &(x, y) in &checkable {
+            let rolls = get_neighbour_rolls(&grid, x, y);
+            if rolls.len() < 4 && grid.get(x, y) == Some(&'@') {
+                remove.push((x, y, rolls));
+            }
+        }
+        checkable.clear();
+    }
+
+    
+    format!("There are {} paper rolls that can be removed", removed)
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,8 @@
+use core::fmt;
+use std::collections::binary_heap::Iter;
 use std::fs;
 use std::path::Path;
+use std::str::FromStr;
 
 /// Reads the input file for a given day and returns its contents as a String.
 pub fn read_input(day: u32) -> String {
@@ -24,8 +27,8 @@ pub fn read_lines(day: u32) -> Vec<String> {
 /// Reads the input file for a given day and parses each line as the specified type.
 pub fn read_parsed<T>(day: u32) -> Vec<T>
 where
-    T: std::str::FromStr,
-    <T as std::str::FromStr>::Err: std::fmt::Debug,
+    T: FromStr,
+    <T as FromStr>::Err: std::fmt::Debug,
 {
     read_lines(day)
         .iter()
@@ -34,4 +37,83 @@ where
             line, err
         )))
         .collect()
+}
+
+pub struct Grid<T> {
+    pub width: usize,
+    pub height: usize,
+    grid: Vec<Vec<T>>
+}
+impl FromStr for Grid<char> {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let grid: Vec<Vec<char>> = s.lines().map(|line| line.chars().collect()).collect();
+        let height = grid.len();
+        let width = grid.get(0).map_or(0, |row| row.len());
+
+        if grid.iter().any(|line| line.len() != width) {
+            Err(String::from("Grid not uniform"))
+        } else {
+            Ok(Self {
+                width,
+                height,
+                grid
+            })
+        }
+    }
+}
+impl fmt::Display for Grid<char> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = self.grid
+            .iter()
+            .map(|row| row.iter().map(|c| format!("{}", c)).collect::<String>())
+            .collect::<Vec<String>>()
+            .join("\n");
+
+        write!(f, "{}", s)
+    }
+}
+impl<T> Grid<T> {
+    pub fn get(&self, x: usize, y: usize) -> Option<&T> {
+        self.grid.get(y)?.get(x)
+    }
+    pub fn get_mut(&mut self, x: usize, y: usize) -> Option<&mut T> {
+        self.grid.get_mut(y)?.get_mut(x)
+    }
+    pub fn set(&mut self, x: usize, y: usize, new_value: T) -> Option<()> {
+        *(self.get_mut(x, y)?) = new_value;
+        Some(())
+    }
+    pub fn iter(&self) -> GridIterator<T> {
+        GridIterator {
+            grid: self,
+            x: 0,
+            y: 0,
+        }
+    }
+}
+
+pub struct GridIterator<'a, T> {
+    grid: &'a Grid<T>,
+    x: usize,
+    y: usize,
+}
+impl<'a, T> Iterator for GridIterator<'a, T> {
+    type Item = (usize, usize, &'a T);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.y >= self.grid.height {
+            return None;
+        }
+
+        let result = self.grid.get(self.x, self.y).map(|value| (self.x, self.y, value));
+
+        self.x += 1;
+        if self.x >= self.grid.width {
+            self.x = 0;
+            self.y += 1;
+        }
+
+        result
+    }
 }
